a73a7e2b7eb55608cef286b901c5b2701acd65ce
sandeepjainicubes <askoog@machinistx.com>
2026-05-27 12:24:27 +0530

    enable railway serverless

--- app.py
+++ app.py
@@ -829,9 +829,9 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 7860))
 
-    demo.launch(
-        server_name="0.0.0.0",
-        server_port=port,
-        share=False,
-        show_error=True,
-    )
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860)),
+    share=False,
+    show_error=True,
+)
